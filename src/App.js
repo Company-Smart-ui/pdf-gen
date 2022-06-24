cb500e0b7d15b4bc00ff6ac6b37febfaec7e695d
Kate Bondarenko <seobootemarket1@gmail.com>
2022-06-24 16:55:29 +0300

    style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {useState} from "react";
 import jsPDF from "jspdf";
+import logo from "./logo.svg";
 
 function App() {
   const [target , setTarget]= useState(100);
@@ -21,16 +22,39 @@ const pdfDownloader =()=>{
 }
 
   return (
-    <div className="App">
-        <h1>Buisnes plan Generator</h1>
-      <input value={name} onChange={(e)=>{setName(e.target.value)}}  placeholder={'enter your business name'} type="text"/>
-      <hr/>
-        <p>coose your target profit profit per month   </p>
-        <input value={target} onChange={(e)=>{setTarget(e.target.value)}} min={1000} max={10000} type="range"/> <span> {target}$</span>
-<hr/>
-        <button onClick={pdfDownloader} className="button"> generate plan</button>
-
+<div className="App">
+  <div className="header container container-lg">
+    <h1><span className="blue">B</span>usiness plan <span className="blue">G</span>enerator</h1>
+    <img src={logo} alt={"logo"} />
+  </div>
+  <div className="container container-xs">
+    <div className="welcome-text">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
+  </div>
+  <div className="generator container container-lg">
+    <div className="generator-text">
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div className="generator-block">
+      <div className="form-block">
+        <label htmlFor={'business-name'}>Enter your business name</label>
+        <input id={'business-name'} value={name} onChange={(e)=>{setName(e.target.value)}}  placeholder="" type="text"/>
+      </div>
+      <div className="form-block">
+        <label htmlFor={'range'}>Coose your target profit per month</label>
+        <div className="range">
+          <input value={target} id={'range'} onChange={(e)=>{setTarget(e.target.value)}} min={1000} max={10000} type="range"/> 
+          <span>{target}$</span>
+        </div>
+        
+      </div>
+      <button onClick={pdfDownloader} className="button"> generate plan</button>
+    </div>
+  </div>
+</div>
   );
 }
 
